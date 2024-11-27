c077712ff134867617728e7265e6cab2fc558dc2
jcy2704 <stevenjchung12@gmail.com>
2024-11-28 03:35:11 +1030

    Add toaster

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
-	import '../app.css';
+	import "../app.css";
+	import { Toaster } from "$lib/components/ui/sonner";
+	import { page } from "$app/stores";
+
+	let protectedRoutes = new Set(["/login", "/registrar", "/password_update"]);
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Toaster position="top-right" richColors />
+
+{#if protectedRoutes.has($page.url.pathname)}
+	{@render children()}
+{:else}
+{/if}
