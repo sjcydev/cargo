ba03556a56695be28a99eff282d7e55ac5e4336f
jcy2704 <stevenjchung12@gmail.com>
2025-08-01 21:11:05 +0930

    Update emails bienvenida to call api

--- src/routes/api/emails/bienvenida/+server.ts
+++ src/routes/api/emails/bienvenida/+server.ts
@@ -1,98 +1,28 @@
 import type { RequestHandler } from "./$types";
-import Bienvenida from "$lib/components/emails/welcome-email.svelte";
-import { db } from "$lib/server/db";
-import { resend } from "$lib/server/resend";
-import { eq } from "drizzle-orm";
-import { sucursales } from "$lib/server/db/schema";
-import { json } from "@sveltejs/kit";
-import { getFriendlyUrl } from "$lib/server/s3";
-import { randomUUID as uuid } from "crypto";
-import { renderAsPlainText } from "svelte-email-tailwind";
-import NuevoCasillero from "$lib/components/emails/new-customer-admin.svelte";
-import { render } from "svelte/server";
+import { API_BASE_URL, API_KEY } from "$env/static/private";
 
 export const POST: RequestHandler = async ({ request }) => {
   const body = await request.json();
 
-  let {
-    nombre,
-    apellido,
-    casillero,
-    sucursalId,
-    correo,
-    cedula,
-    telefono,
-    reenviar = false,
-  } = body;
-
   try {
-    const company = await db.query.companies.findFirst()!;
-
-    const sucursal = await db.query.sucursales.findFirst({
-      where: eq(sucursales.sucursalId, Number(sucursalId)),
-    });
-
-    if (company) {
-      const logo = getFriendlyUrl(company.logo!);
-
-      const compProps = {
-        nombre,
-        apellido,
-        casillero,
-        sucursal: sucursal?.sucursal,
-        codigo_de_compania: sucursal?.codificacion,
-        nombre_de_compania: company.company,
-        logo,
-      };
-
-      const { body: emailHtml } = render(Bienvenida, { props: compProps });
-
-      const emailText = await renderAsPlainText(emailHtml);
-
-      const data = await resend.emails.send({
-        from: `${company.company} <no-reply-info@${company.dominio}>`,
-        to: [correo],
-        subject: `¡Bienvenido a ${company.company}!`,
-        html: emailHtml,
-        text: emailText,
+    if (body) {
+      const res = await fetch(`${API_BASE_URL}/emails/bienvenida`, {
+        method: "POST",
         headers: {
-          "X-Entity-Ref-ID": uuid(),
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${API_KEY}`,
         },
+        body: JSON.stringify(body),
       });
 
-      if (!reenviar) {
-        const { body: adminEmailHtml } = render(NuevoCasillero, {
-          props: {
-            nombre,
-            apellido,
-            casillero,
-            sucursal: sucursal?.sucursal,
-            codigo_de_compania: sucursal?.codificacion,
-            logo,
-            correo,
-            cedula,
-            telefono,
-          },
-        });
-
-        const adminEmailText = await renderAsPlainText(adminEmailHtml);
-
-        await resend.emails.send({
-          from: `${company.company} <no-reply-info@${company.dominio}>`,
-          to: [sucursal!.correo],
-          subject: `¡Nuevo Casillero Registrado!`,
-          html: adminEmailHtml,
-          text: adminEmailText,
-        });
+      if (!res.ok) {
+        throw new Error("Error in the server");
       }
-
-      return json({ data, message: "Email sent successfully" });
     }
-  } catch (error) {
-    console.error(error);
-    return new Response(JSON.stringify({ error: "Failed to send email" }), {
-      status: 500,
-    });
+
+    return new Response(JSON.stringify({ success: true }), { status: 200 });
+  } catch (e) {
+    console.error(e);
   }
 
   return new Response(JSON.stringify({ error: "Company not found" }), {
